696e3425fe62c4af40014162b5ad2ca6ae9f39aa
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-05 23:06:38 +0100

    added dummy test
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/back-end/libToken/specs/remove-it-later.spec.mjs
@@ -0,0 +1,16 @@
+import mocha from 'mocha';
+import chai from 'chai';
+
+const {
+  describe,
+  it,
+} = mocha;
+const {
+  expect,
+} = chai;
+
+describe('remove', () => {
+  it('should be removed when proper tests emerge', async () => {
+    expect(true).to.be.true;
+  });
+});
