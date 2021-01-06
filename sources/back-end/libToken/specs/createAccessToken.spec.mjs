c424a754a83886d24a5e8a8e435a3604f5ca8587
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-06 17:56:26 +0100

    WIP: AccessToken
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/back-end/libToken/specs/createAccessToken.spec.mjs
@@ -0,0 +1,17 @@
+import mocha from 'mocha';
+import chai from 'chai';
+import _sodium from 'libsodium-wrappers';
+import macaroons from 'macaroons.js';
+
+const {
+  describe,
+  before,
+  it,
+} = mocha;
+const {
+  expect,
+} = chai;
+
+describe('AccessToken', () => {
+  
+});
