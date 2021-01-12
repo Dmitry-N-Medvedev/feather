6b7cf3789c7815153e281b67ca52f8d77ed5e7c2
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-12 14:32:32 +0100

    update node version
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/back-end/libToken/constants/ActionTypes.mjs
+++ sources/back-end/libToken/constants/ActionTypes.mjs
@@ -1,5 +1,5 @@
 export const ActionTypes = Object.freeze({
   READ: 0x01, // read - GET a file
-  WRITE: 0x02, // write - not used. Should be used to change a file on the server.
-  EXECUTE: 0x04, // execute - POST results
+  WRITE: 0x02, // write - POST, PUT, PATCH a file.
+  EXECUTE: 0x04, // execute - call a server-side function which does not involve changing a file.
 });
