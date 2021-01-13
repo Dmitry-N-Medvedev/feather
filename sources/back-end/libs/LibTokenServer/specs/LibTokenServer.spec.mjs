b50b30100fdc31463e63193aec0214c7629eccb2
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-13 17:17:52 +0100

    added more test coverage hopefully
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/back-end/libs/LibTokenServer/specs/LibTokenServer.spec.mjs
+++ sources/back-end/libs/LibTokenServer/specs/LibTokenServer.spec.mjs
@@ -3,6 +3,9 @@ import mocha from 'mocha';
 import chai from 'chai';
 import macaroons from 'macaroons.js';
 import {
+  nanoid,
+} from 'nanoid';
+import {
   LibRedisAdapter,
 } from '@dmitry-n-medvedev/libredisadapter/LibRedisAdapter.mjs';
 import {
@@ -14,6 +17,12 @@ import {
 import {
   LibTokenServer,
 } from '../LibTokenServer.mjs';
+import {
+  resolveTokenIdentifierName,
+} from '../helpers/resolveTokenIdentifierName.mjs';
+import {
+  validateAction,
+} from '../authz/validateAction.mjs';
 
 const debuglog = util.debuglog('specs');
 const {
@@ -119,5 +128,122 @@ describe('LibTokenServer', () => {
     } = deserializedAccessToken;
 
     keysToDelete.push(identifier);
+  });
+
+  it('should fail to issueAccessToken w/ undefined forAction', async () => {
+    const forAction = null;
+    const serializedAccountToken = await libTokenServer.issueAccountToken();
+
+    let error = null;
+
+    try {
+      await libTokenServer.issueAccessToken(forAction, serializedAccountToken, Locations.FILE_SERVER);
+    } catch (forActionError) {
+      error = forActionError;
+    }
+
+    expect(error).to.be.an.instanceof(ReferenceError);
+  });
+
+  it('should fail to issueAccessToken w/ undefined accountToken', async () => {
+    const forAction = Object.freeze({
+      type: ActionTypes.READ,
+      object: '/questionnaire.json',
+    });
+    const undefinedAccountToken = null;
+
+    let error = null;
+
+    try {
+      await libTokenServer.issueAccessToken(forAction, undefinedAccountToken, Locations.FILE_SERVER);
+    } catch (forActionError) {
+      error = forActionError;
+    }
+
+    expect(error).to.be.an.instanceof(ReferenceError);
+  });
+
+  it('undefined Location should lead to an Access Token with Locations.INVALID_LOCATION', async () => {
+    const forAction = Object.freeze({
+      type: ActionTypes.READ,
+      object: '/questionnaire.json',
+    });
+    const undefinedLocation = null;
+    const serializedAccountToken = await libTokenServer.issueAccountToken();
+
+    const serializedAccessToken = await libTokenServer.issueAccessToken(forAction, serializedAccountToken, undefinedLocation);
+    const deserializedAccessToken = MacaroonsBuilder.deserialize(serializedAccessToken);
+
+    const {
+      location,
+    } = deserializedAccessToken;
+
+    expect(location).to.equal(Locations.INVALID_LOCATION.toString());
+  });
+
+  it('should check resolveTokenIdentifierName', async () => {
+    const validArgs = Object.freeze([
+      [nanoid(5), nanoid(5)],
+    ]);
+    const invalidArgs = Object.freeze([
+      [nanoid(5), null],
+      [null, nanoid(5)],
+      [null, null],
+    ]);
+
+    for (const [tokenType, forIdentifier] of validArgs) {
+      const name = resolveTokenIdentifierName(tokenType, forIdentifier);
+
+      expect(name.length > 0).to.be.true;
+    }
+
+    for (const [tokenType, forIdentifier] of invalidArgs) {
+      let error = null;
+
+      try {
+        resolveTokenIdentifierName(tokenType, forIdentifier);
+      } catch (nameError) {
+        error = nameError;
+      }
+
+      expect(error).to.be.an.instanceof(ReferenceError);
+    }
+  });
+
+  it('should validateAction', async () => {
+    const validParameters = Object.freeze([
+      [nanoid(), {
+        type: ActionTypes.READ,
+        object: '/questionnaire.json',
+      }],
+    ]);
+    const invalidParameters = Object.freeze([
+      [nanoid(), null],
+      [null, {
+        type: ActionTypes.READ,
+        object: '/questionnaire.json',
+      }],
+      [null, null],
+    ]);
+
+    for (const [userId, action] of validParameters) {
+      const resolvedAction = validateAction(userId, action);
+
+      expect(resolvedAction).to.deep.equal(action);
+    }
+
+    for (const [userId, action] of invalidParameters) {
+      let error = null;
+
+      try {
+        const resolvedAction = validateAction(userId, action);
+
+        expect(resolvedAction).to.not.exist;
+      } catch (validationError) {
+        error = validationError;
+      }
+
+      expect(error).to.be.an.instanceof(ReferenceError);
+    }
   });
 });
