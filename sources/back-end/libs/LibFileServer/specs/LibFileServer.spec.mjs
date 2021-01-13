e70844e62843e4859ca570eb226eadd812fbc218
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-14 00:48:08 +0100

    FileServer
    1. added initial code. NOT WORKING YET
    
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/back-end/libs/LibFileServer/specs/LibFileServer.spec.mjs
@@ -0,0 +1,35 @@
+import mocha from 'mocha';
+import chai from 'chai';
+import util from 'util';
+import {
+  resolve,
+} from 'path';
+import {
+  LibFileServer,
+} from '../LibFileServer.mjs';
+
+const debuglog = util.debuglog('LibFileServer:specs');
+const {
+  describe,
+  it,
+} = mocha;
+const {
+  expect,
+} = chai;
+
+describe('LibFileServer', () => {
+  it.only('should start/stop the LibFileServer', async () => {
+    const LibFileServerConfig = Object.freeze({
+      cwd: resolve(process.cwd(), './specs/cwd'),
+    });
+    const libFileServer = new LibFileServer(LibFileServerConfig);
+
+    expect(libFileServer).to.exist;
+
+    await libFileServer.start();
+
+    expect(libFileServer.files).is.frozen;
+
+    await libFileServer.stop();
+  });
+});
