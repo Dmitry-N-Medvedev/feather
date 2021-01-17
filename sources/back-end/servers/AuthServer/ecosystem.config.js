095f56e33de52ac87982f8a7ec1ec3f0256fd901
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-17 16:45:04 +0100

    WIP: before publishing to verdaccio
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/back-end/servers/AuthServer/ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: 'auth server',
+      script: './index.mjs',
+      instances: 2,
+      exec_mode: 'cluster',
+      increment_var: 'UWS_PORT',
+      time: true,
+      combine_logs: true,
+      env: {
+        NODE_ENV: 'development',
+        NODE_DEBUG: 'AuthServer*,LibAuthServer*',
+        REDIS_HOST: '127.0.0.1',
+        REDIS_PORT: 6379,
+        UWS_PORT: 9090,
+      },
+      env_production: {
+        NODE_DEBUG: 'AuthServer*,LibAuthServer*',
+        REDIS_HOST: '127.0.0.1',
+        REDIS_PORT: 6379,
+        UWS_PORT: 9090,
+      },
+      watch: false,
+    },
+  ],
+};
