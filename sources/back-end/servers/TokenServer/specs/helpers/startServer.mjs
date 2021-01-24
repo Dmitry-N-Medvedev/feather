86ff626d89fce950ad8d7cd6989d49aaaa9270f7
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-24 13:04:13 +0100

    added a test for the TokenServer
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/back-end/servers/TokenServer/specs/helpers/startServer.mjs
@@ -0,0 +1,17 @@
+const re = /started\son\sport\s(\d+)$/gmiu;
+
+export const startServer = (logger = null) => new Promise((resolve, reject) => {
+  if (logger === null) {
+    return reject(new ReferenceError('logger is undefined'));
+  }
+
+  logger.on('data', (data) => {
+    const match = re.exec(data.toString()) ?? null;
+
+    if (match !== null) {
+      resolve();
+    }
+  });
+
+  return undefined;
+});
---
+++ sources/back-end/servers/TokenServer/specs/helpers/stopServer.mjs
@@ -0,0 +1,21 @@
+export const stopServer = (handle = null, logger = null) => new Promise((resolve, reject) => {
+  if (handle === null) {
+    reject(new ReferenceError('handle is undefined'));
+  }
+
+  if (logger === null) {
+    reject(new ReferenceError('logger is undefined'));
+  }
+
+  logger.on('data', (data) => {
+    console.debug(data.toString());
+    // const match = re.exec(data.toString()) ?? null;
+
+    // if (match !== null) {
+    //   resolve();
+    // }
+    resolve();
+  });
+
+  handle.cancel();
+});
