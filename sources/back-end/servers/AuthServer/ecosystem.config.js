f52c3fa1ba74f663b1a9e8841343d5c2182c1e82
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-25 18:56:41 +0100

    adding docker test
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/back-end/servers/AuthServer/ecosystem.config.js
+++ sources/back-end/servers/AuthServer/ecosystem.config.js
@@ -10,10 +10,6 @@ module.exports = {
       combine_logs: true,
       env: {
         NODE_ENV: 'development',
-        NODE_DEBUG: 'AuthServer*,LibAuthServer*',
-        REDIS_HOST: 'redis-server',
-        REDIS_PORT: 6379,
-        UWS_PORT: 80,
       },
       watch: false,
     },
--- sources/back-end/servers/TokenServer/ecosystem.config.js
+++ sources/back-end/servers/TokenServer/ecosystem.config.js
@@ -9,12 +9,6 @@ module.exports = {
       combine_logs: true,
       env: {
         NODE_ENV: 'development',
-        NODE_DEBUG: 'Lib*,Token*',
-        CONTEXT: 'featheri',
-        REDIS_HOST: 'redis-server',
-        REDIS_PORT: 6379,
-        TTL: 500,
-        UWS_PORT: 9091,
       },
       watch: false,
     },
