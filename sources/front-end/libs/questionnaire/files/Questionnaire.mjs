0b058ef7fe98df7f6b2dfe80c978b69f243bc804
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-29 02:31:55 +0100

    WIP: Questionnaire
    seems like an example of a questionnaire is done.
    
    TODO:
    1. replace a json file on file server with this Questionnaire.mjs
    2. setup the mime type for mjs files on NGINX, remove json definition
    3. the client test should request the Questionnaire.mjs, load it via fetch, and import it via "data:"
    
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/front-end/libs/questionnaire/files/Questionnaire.mjs
+++ sources/front-end/libs/questionnaire/files/Questionnaire.mjs
@@ -1,7 +1,8 @@
-import {
-  createMachine,
-  interpret,
-} from 'xstate';
+// import {
+//   createMachine,
+//   interpret,
+//   assign,
+// } from 'xstate';
 
 /*
 
@@ -17,29 +18,462 @@ import {
 
 */
 
-const Questionnaire = createMachine({
+const validateString = (context, event) => new Promise((resolve, reject) => {
+  if (event.type !== 'reply') {
+    return reject(new TypeError(`event type "${event.type}" was not expected`));
+  }
+
+  const {
+    payload: {
+      id,
+      value,
+    },
+  } = event;
+
+  const {
+    response: {
+      type,
+      validation,
+    },
+  } = context.questions[id];
+
+  if (type !== 'string') {
+    return reject(new TypeError(`wrong validator: type ${type} is not a string`));
+  }
+
+  if (typeof value !== 'string') {
+    return reject(new TypeError(`expected ${value} to be a string`));
+  }
+
+  if ('length' in validation) {
+    if (value.length < validation.length.min || value.length > validation.length.max) {
+      return reject(new EvalError(
+        `the length of the ${value} (${value.length}) is outside of the acceptable boundary: [${validation.length.min}..${validation.length.max}]`,
+      ));
+    }
+  }
+
+  return resolve();
+});
+
+const validateBoolean = (context, event) => new Promise((resolve, reject) => {
+  if (event.type !== 'reply') {
+    return reject(new TypeError(`event type "${event.type}" was not expected`));
+  }
+
+  const {
+    payload: {
+      id,
+      value,
+    },
+  } = event;
+
+  const {
+    response: {
+      type,
+    },
+  } = context.questions[id];
+
+  if (type !== 'boolean') {
+    return reject(new TypeError(`wrong validator: type ${type} is not a boolean`));
+  }
+
+  if (typeof value !== 'boolean') {
+    return reject(new TypeError(`expected ${value} to be a boolean`));
+  }
+
+  return resolve();
+});
+
+const validateInt = (context, event) => new Promise((resolve, reject) => {
+  if (event.type !== 'reply') {
+    return reject(new TypeError(`event type "${event.type}" was not expected`));
+  }
+
+  const {
+    payload: {
+      id,
+      value,
+    },
+  } = event;
+
+  const {
+    response: {
+      type,
+      validation: {
+        min,
+        max,
+      },
+    },
+  } = context.questions[id];
+
+  if (type !== 'integer') {
+    return reject(new TypeError(`wrong validator: type ${type} is not an integer`));
+  }
+
+  if (Number.isInteger(value) === false) {
+    return reject(new TypeError(`the ${value} is expected to be an integer`));
+  }
+
+  if (value < min || value > max) {
+    return reject(
+      new EvalError(`${value} is outside of the acceptable boundary: [${min}..${max}]`),
+    );
+  }
+
+  return resolve();
+});
+
+const responderHasChildren = (context) => context.answers.haveChildren === true;
+
+const allChildrenDetailsCollected = (context) => {
+  const result = context.answers.childrenDetails.length === context.answers.howManyChildren;
+
+  // console.log('[guard] allChildrenDetailsCollected:', result, context.answers, context.answers.howManyChildren);
+
+  return result;
+};
+
+const createQuestionnaire = (xstate, askQuestion) => xstate.createMachine({
   id: 'Questionnaire',
   context: {
-    initialQuestionId: '0',
     questions: {
-      0: {
-        question: {
-          text: 'First name',
+      firstName: {
+        id: 'firstName',
+        question: 'what is your first name',
+        response: {
+          type: 'string',
+          validation: {
+            length: {
+              min: 2,
+              max: 20,
+            },
+          },
+        },
+      },
+      address: {
+        id: 'address',
+        question: 'where do you live',
+        response: {
+          type: 'string',
+          validation: {
+            length: {
+              min: 10,
+              max: 250,
+            },
+          },
+        },
+      },
+      haveChildren: {
+        id: 'haveChildren',
+        question: 'do you have children',
+        response: {
+          type: 'boolean',
+        },
+      },
+      howManyChildren: {
+        id: 'howManyChildren',
+        question: 'how many children do you have',
+        response: {
+          type: 'integer',
+          validation: {
+            min: 1,
+            max: Number.MAX_SAFE_INTEGER,
+          },
+        },
+      },
+      childOccupation: {
+        id: 'childOccupation',
+        question: 'your child occupation',
+        response: {
+          type: 'string',
+          validation: {
+            allowedValues: ['employed', 'student', 'self-employed'],
+          },
+        },
+      },
+      emailAddress: {
+        id: 'emailAddress',
+        question: 'what is your email address',
+        response: {
+          type: 'string',
+          validation: {},
         },
       },
     },
-    answers: {},
+    answers: {
+      childrenDetails: [],
+    },
   },
-  initial: 'initial',
+  initial: 'firstName',
   states: {
-    initial: {},
+    firstName: {
+      entry: [
+        (context) => {
+          askQuestion({
+            type: 'question',
+            payload: {
+              value: context.questions.firstName,
+            },
+          });
+        },
+      ],
+      on: {
+        reply: {
+          actions: [
+            xstate.assign({
+              answers: (context, event) => {
+                context.answers = {
+                  ...context.answers,
+                  [event.payload.id]: event.payload.value,
+                };
+
+                return context.answers;
+              },
+            }),
+          ],
+          target: ['validateFirstName'],
+        },
+      },
+    },
+    validateFirstName: {
+      invoke: {
+        id: 'validateFirstName',
+        src: validateString,
+        onDone: {
+          target: 'address',
+        },
+        onError: {
+          target: 'firstName',
+        },
+      },
+    },
+    address: {
+      entry: [
+        (context) => {
+          askQuestion({
+            type: 'question',
+            payload: {
+              value: context.questions.address,
+            },
+          });
+        },
+      ],
+      on: {
+        reply: {
+          actions: [
+            xstate.assign({
+              answers: (context, event) => {
+                context.answers = {
+                  ...context.answers,
+                  [event.payload.id]: event.payload.value,
+                };
+
+                return context.answers;
+              },
+            }),
+          ],
+          target: ['validateAddress'],
+        },
+      },
+    },
+    validateAddress: {
+      invoke: {
+        id: 'validateAddress',
+        src: validateString,
+        onDone: {
+          target: 'haveChildren',
+        },
+        onError: {
+          target: 'address',
+        },
+      },
+    },
+    haveChildren: {
+      entry: [
+        (context) => {
+          askQuestion({
+            type: 'question',
+            payload: {
+              value: context.questions.haveChildren,
+            },
+          });
+        },
+      ],
+      on: {
+        reply: {
+          actions: [
+            xstate.assign({
+              answers: (context, event) => {
+                context.answers = {
+                  ...context.answers,
+                  [event.payload.id]: event.payload.value,
+                };
+
+                return context.answers;
+              },
+            }),
+          ],
+          target: ['validateHaveChildren'],
+        },
+      },
+    },
+    validateHaveChildren: {
+      invoke: {
+        id: 'validateHaveChildren',
+        src: validateBoolean,
+        onDone: {
+          target: 'shouldAskForChildrenDetails',
+        },
+        onError: {
+          target: 'haveChildren',
+        },
+      },
+    },
+    shouldAskForChildrenDetails: {
+      always: [
+        {
+          target: 'howManyChildren',
+          cond: {
+            type: 'responderHasChildren',
+          },
+        },
+        {
+          target: 'askForEmailAddress',
+        },
+      ],
+    },
+    howManyChildren: {
+      entry: [
+        (context) => {
+          askQuestion({
+            type: 'question',
+            payload: {
+              value: context.questions.howManyChildren,
+            },
+          });
+        },
+      ],
+      on: {
+        reply: {
+          actions: [
+            xstate.assign({
+              answers: (context, event) => {
+                context.answers = {
+                  ...context.answers,
+                  [event.payload.id]: event.payload.value,
+                };
+
+                return context.answers;
+              },
+            }),
+          ],
+          target: ['validateHowManyChildren'],
+        },
+      },
+    },
+    validateHowManyChildren: {
+      invoke: {
+        id: 'validateHowManyChildren',
+        src: validateInt,
+        onDone: {
+          target: 'askForChildrenDetails',
+        },
+        onError: {
+          target: 'howManyChildren',
+        },
+      },
+    },
+    askForChildrenDetails: {
+      always: [
+        {
+          target: 'askForEmailAddress',
+          cond: 'allChildrenDetailsCollected',
+        },
+        {
+          target: 'askForChildOccupation',
+        },
+      ],
+    },
+    askForChildOccupation: {
+      entry: [
+        (context) => {
+          askQuestion({
+            type: 'question',
+            payload: {
+              value: context.questions.childOccupation,
+            },
+          });
+        },
+      ],
+      on: {
+        reply: {
+          actions: [
+            xstate.assign({
+              answers: (context, event) => {
+                context.answers = {
+                  ...context.answers,
+                  ...{
+                    childrenDetails: [...context.answers.childrenDetails, event.payload.value],
+                  },
+                };
+
+                return context.answers;
+              },
+            }),
+          ],
+          target: ['askForChildrenDetails'],
+        },
+      },
+    },
+    askForEmailAddress: {
+      entry: [
+        (context) => {
+          askQuestion({
+            type: 'question',
+            payload: {
+              value: context.questions.emailAddress,
+            },
+          });
+        },
+      ],
+      on: {
+        reply: {
+          actions: [
+            xstate.assign({
+              answers: (context, event) => {
+                context.answers = {
+                  ...context.answers,
+                  [event.payload.id]: event.payload.value,
+                };
+
+                return context.answers;
+              },
+            }),
+          ],
+          target: ['final_OK'],
+        },
+      },
+    },
+    final_OK: {
+      type: 'final',
+      data: (context) => context.answers,
+    },
+    final_ER: {
+      type: 'final',
+    },
   },
 }, {
   actions: {},
   activities: {},
   delays: {},
-  guards: {},
+  guards: {
+    responderHasChildren,
+    allChildrenDetailsCollected,
+  },
   services: {},
 });
 
-export default interpret(Questionnaire);
+export default (xstate, askQuestion) => xstate.interpret(createQuestionnaire(xstate, askQuestion), {
+  execute: true,
+});
--- sources/front-end/libs/questionnaire/specs/Questionnaire.spec.mjs
+++ sources/front-end/libs/questionnaire/specs/Questionnaire.spec.mjs
@@ -1,6 +1,9 @@
 import util from 'util';
 import mocha from 'mocha';
 import chai from 'chai';
+import xstate from 'xstate';
+
+import Questionnaire from '../files/Questionnaire.mjs';
 
 const debuglog = util.debuglog('Questionnaire:specs');
 const {
@@ -13,7 +16,104 @@ const {
 
 // FIXME: remove this when everything is ready
 describe('Questionnaire', () => {
-  it.only('should conduct the Questionnaire', async () => {
+  it.only('should conduct the Questionnaire', () => new Promise((resolve, reject) => {
+    let questionnaire = null;
+    const expectedReplies = Object.freeze({
+      firstName: 'Nikolay',
+      address: 'Lohmühlenstraße 65, 12435 Berlin',
+      haveChildren: true,
+      howManyChildren: Math.floor(Math.random() * 2 + 2),
+      emailAddress: 'hello@feather-insurance.com',
+    });
+
+    const receiveQuestion = (question) => {
+      switch (question.payload.value.id) {
+        case 'firstName': {
+          questionnaire.send({
+            type: 'reply',
+            payload: {
+              id: question.payload.value.id,
+              value: expectedReplies.firstName,
+            },
+          });
+
+          return resolve();
+        }
+        case 'address': {
+          questionnaire.send({
+            type: 'reply',
+            payload: {
+              id: question.payload.value.id,
+              value: expectedReplies.address,
+            },
+          });
+
+          return resolve();
+        }
+        case 'haveChildren': {
+          questionnaire.send({
+            type: 'reply',
+            payload: {
+              id: question.payload.value.id,
+              value: expectedReplies.haveChildren,
+            },
+          });
+
+          return resolve();
+        }
+        case 'howManyChildren': {
+          questionnaire.send({
+            type: 'reply',
+            payload: {
+              id: question.payload.value.id,
+              value: expectedReplies.howManyChildren,
+            },
+          });
+
+          return resolve();
+        }
+        case 'childOccupation': {
+          const randomOccupation = question.payload.value.response.validation.allowedValues[
+            Math.floor(Math.random() * Math.floor(question.payload.value.response.validation.allowedValues.length))
+          ];
+
+          questionnaire.send({
+            type: 'reply',
+            payload: {
+              id: question.payload.value.id,
+              value: randomOccupation,
+            },
+          });
+
+          return resolve();
+        }
+        case 'emailAddress': {
+          questionnaire.send({
+            type: 'reply',
+            payload: {
+              id: question.payload.value.id,
+              value: expectedReplies.emailAddress,
+            },
+          });
+
+          return resolve();
+        }
+        default: {
+          return reject(new TypeError('unknown question'));
+        }
+      }
+    };
+
+    questionnaire = Questionnaire(xstate, receiveQuestion);
+
+    questionnaire
+      .onDone((data) => {
+        expect(data).to.deep.equal(expectedReplies);
+
+        resolve();
+      })
+      .start();
+
     expect(true).to.be.true;
-  });
+  }));
 });
